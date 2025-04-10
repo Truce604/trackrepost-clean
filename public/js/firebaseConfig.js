b1d93ffc01e0e3db7e4d95cf636fb281bcdaf25a
Truce604 <JordanLilwall@gmail.com>
2025-04-09 18:13:45 -0600

    🚀 Force add firebaseConfig.js for production

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,20 @@
+// ✅ TrackRepost Firebase Config (Compat)
+window.firebaseConfig = {
+  apiKey: "AIzaSyAGmhdeSxshYSmaAbsMtda4qa1K3TeKiYw",
+  authDomain: "trackrepost-921f8.firebaseapp.com",
+  projectId: "trackrepost-921f8",
+  storageBucket: "trackrepost-921f8.appspot.com",
+  messagingSenderId: "967836604288",
+  appId: "1:967836604288:web:3782d50de7384c9201d365",
+  measurementId: "G-G65Q3HC3R8"
+};
+
+// ✅ Square public frontend keys
+window.SQUARE_APP_ID = "EAAAl_xqHJGyrWyUE53BgNQZoGbjI64lplndYyxNXAkv--1dx7owzl14oq64aHwp";
+window.SQUARE_LOCATION_ID = "sq0idp-PgaanSd67uGXtHuBFn7cZA";
+
+console.log("✅ Firebase config loaded:", window.firebaseConfig);
+
+
+
+  
